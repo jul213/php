d519189fe16ffaf60ccf9fe41254c55e0e5a2e84
jul213 <158296107+jul213@users.noreply.github.com>
2025-01-11 22:04:30 +0100

    Update notas.php
    modificcacion de clase estudiante

--- notas.php
+++ notas.php
@@ -6,49 +6,35 @@ class estudiante {
     private $nombre;
     private $edad;
     private $curso;
-    private $periodos;
+    private $notas = [];
     
     
-    function __construct($nombre,$edad,$curso) {
+    public function __construct($nombre,$edad,$curso) {
         $this->nombre = $nombre;
         $this->edad = $edad;
         $this->curso = $curso;
     }
     
     
-    public function curso($curso) {
-        $this-> curso = $curso;
-        
-        switch ($curso) {
-            
-            case "informatica":
-                echo "es informatico";
-                break;
-            case "programador":
-                echo "eres un programador";
-            
-            case "redes":
-                echo "arquitecto de red";
-                break;
-            
-            default:
-                echo "estudias otra cosa";
-                
+   public function getNombre() {
+        return $this->nombre;
         }
-    }
-        
-        
-        public function setPeriodos($periodos) {
-            $this->periodos = $periodos;
-        }
-        
-        public function notas($nota1,$nota2,$nota3){
-            
-            $resultado = $nota1+$nota2+$nota3;
-            
-            $promedio = $resultado/$this->periodos;
-            
-            return $promedio;
+
+    public function getCurso() {
+        return $this->curso;
+         }
+
+        public function setCurso($curso) {
+            $this->curso = $curso;
+     }
+
+    public function getNotas() {
+         return $this->$notas;
+         }
+
+    protected function setNotas($notas) {
+        $this->notas = $notas
+         }
             
             
             
