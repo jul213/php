289cd90e243d1533cc7421deccf2d9feea7cd3ae
jul213 <158296107+jul213@users.noreply.github.com>
2025-01-11 22:50:16 +0100

    Update notas.php
    ampliacion del codigo

--- notas.php
+++ notas.php
@@ -40,6 +40,43 @@ class estudiante {
             
         }
     }
+
+
+
+
+
+
+
+
+class Director {
+    private $erasmus = 0;
+    public function AsignarCurso(Estudiante $estudiante, $curso) {
+        $estudiante->setCurso($curso);
+        $this->erasmus = 0;
+         }
+
+    switch ($curso) {
+    case "informatica":
+    case "diseño grafico":
+    case "derecho":
+    case "marketing":
+    case "administracion":
+    case "economia":
+    case "enfermeria":
+    case "medicina":
+    case "arquitectura":
+    case "informatica":
+     case "fisica":
+        echo "eres de" . $curso . " estas en la lista para poder hacer erasmus"; 
+        $this->erasmus = 1;
+        break;
+    default:
+        echo "lo que estudias no esta en la lista para poder hacer erasmus"
+         $this->erasmus = 0;
+            
+    
+     }
+     }
     
 
 ?>
