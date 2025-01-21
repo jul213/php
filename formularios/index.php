fe1f6cf78a5f96c0b5df8163ee7b16a2f35c6f4d
jul213 <158296107+jul213@users.noreply.github.com>
2025-01-21 17:52:19 +0000

    inicio del foreach

--- formularios/index.php
+++ formularios/index.php
@@ -1,9 +1,8 @@
 <?php
 
-$dispositivo = $_POST["dispositivo"];
 $opcion1 = $_POST["si"];
 $opcion2 = $_POST["no"];
+foreach($_POST["dispositivo"] as $dispositivo){
 
-
-echo "tu informacion es: tienes un -> ${dispoitivo} y tu preferencia ante la programacion es: ${opcion1}";
+}
 ?>
