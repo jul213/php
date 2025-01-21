e99bc78c8203adcf404b8a2e94a83426630410d7
jul213 <158296107+jul213@users.noreply.github.com>
2025-01-21 17:48:33 +0000

    terminar de mmmanejar informmmacion del formulario

--- formularios/index.php
+++ formularios/index.php
@@ -1,5 +1,9 @@
 <?php
 
 $dispositivo = $_POST["dispositivo"];
+$opcion1 = $_POST["si"];
+$opcion2 = $_POST["no"];
 
+
+echo "tu informacion es: tienes un -> ${dispoitivo} y tu preferencia ante la programacion es: ${opcion1}";
 ?>
