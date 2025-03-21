153ee12796c54c6a35250cc27fffb2eef7a1b52e
julio <158296107+jul213@users.noreply.github.com>
2025-03-21 05:52:30 +0000

    creacion de variables con valores vacios

---
+++ formularios/formValidate.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario</title>
+</head>
+<body>
+    <?php 
+
+        $name = $email = $gender = $comment = $website = "";
+    ?>
+</body>
+</html>
