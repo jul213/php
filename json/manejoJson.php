37a560cb932357d73cb80378d01b57b4011cbf11
julio <158296107+jul213@users.noreply.github.com>
2025-03-22 23:39:45 +0000

    decodificamos el json para mstrarlo en formato mas amigable

--- json/manejoJson.php
+++ json/manejoJson.php
@@ -3,3 +3,18 @@ $array = array("buenas", "saludos", "adios", "hasta luego");
 
 echo json_encode($array);
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>mostramos formato amigable Json</title>
+</head>
+<body>
+    <?php 
+    echo json_decode($array);
+    ?>
+</body>
+</html>
