20173ab9d21d5997270443ae5bb49af9a3fb5c2f
julio <158296107+jul213@users.noreply.github.com>
2025-03-22 22:04:43 +0000

    comprobacion de la cookie si ya fue seteada

--- cookies/cookie.php
+++ cookies/cookie.php
@@ -3,3 +3,14 @@ $cookie_name = "user";
 $cookie_value = "Pablo";
 setcookie($cookie_name, $cookie_value, time() + (8400 * 30),"/");
 ?>
+
+<html>
+    <body>
+        
+        <?php 
+            if (!isset($_COOKIE[$cookie_name])){
+                echo "cookie named: " . $cookie_name . "is not set";
+            }
+        ?>
+    </body>
+</html>
