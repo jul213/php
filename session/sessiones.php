bfcac228d4d9cc2fb35cc41feb2249e87fee5c8e
julio <158296107+jul213@users.noreply.github.com>
2025-03-22 22:22:28 +0000

    cambiamos una variable de la session

--- session/sessiones.php
+++ session/sessiones.php
@@ -1,3 +1,17 @@
 <?php 
 session_start();
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sesiones con php</title>
+</head>
+<body>
+    <?php 
+    $_SESSION["favcolor"] = "yellow";
+    ?>
+</body>
+</html>
