10b9a492841074fb4f487bad642bfa05bdeed61e
julio <158296107+jul213@users.noreply.github.com>
2025-03-24 09:33:32 +0000

    creacion de clase cat e implementacion de la interfaz animal

--- OOP/interfaces.php
+++ OOP/interfaces.php
@@ -3,4 +3,11 @@
     interface Animal {
         public function sonido();
     }
+
+
+    class Cat implements Animal {
+        public function sonido(){
+            echo "Meow";
+        }
+    }
 ?>
