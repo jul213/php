b1c5f4de9080620cfb2b9ea0fd3e6f11c45b5f76
julio <158296107+jul213@users.noreply.github.com>
2025-03-25 11:11:28 +0000

    creacion de clase MyIterator que implementa la interfaz

--- OOP/iterables.php
+++ OOP/iterables.php
@@ -18,3 +18,20 @@
     }
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Creamos un Iterador</title>
+</head>
+<body>
+    <?php 
+    
+        class MyIterator implements Iterator {
+            
+        }
+    ?>
+</body>
+</html>
